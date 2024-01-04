93af4639f5693ec7e7e743da279ba22aa4601960
edit-grcodeclub <155640228+edit-grcodeclub@users.noreply.github.com>
2024-01-04 14:38:55 +0200

    Create menu.js

---
+++ js/menu.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    
+    var menuLink = ' <li><a href="https://edit-grcodeclub.github.io/">Αρχική Σελίδα</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+
+    menuLink = ' <li><a href="#">Στοιχεία</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    
+ 
+    menuLink = '<li style="height: 200px;"></li>';
+    $('.sidebarMenuInner').append(menuLink);
+  });
+  
